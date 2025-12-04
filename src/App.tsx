b769d5be2a6cbd0dd87225b3bcf4da3d5161f26a
Henrique Barros <henrique.correia@fbter.org.br>
2025-12-04 20:19:19 -0300

    Fix login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
 import "./App.css";
-import "./index.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import AboutTTG from "./components/shared/AboutTTG";
+
+import LoginForm from "./components/shared/loginForm"; 
+import logoCimatec from "./assets/cimatec-universidade-logo.png";
+import { Link } from "react-router-dom";
 
 function App() {
+  
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/sobre-ttg" element={<AboutTTG />} />
-      </Routes>
-    </Router>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center gap-12 bg-[url('./assets/login-bg.png')] bg-cover bg-center bg-no-repeat overflow-hidden">
+      
+      <header className="flex flex-col items-center gap-2">
+        <p className="text-lg font-light text-gray-200 subpixel-antialiased">
+          Portal de Projetos
+        </p>
+        <h1 className="font-rift text-6xl font-bold text-green-400 subpixel-antialiased text-center px-4">
+          TRILHA TÉCNICO-GESTOR
+        </h1>
+      </header>
+
+      <main className="flex w-3/4 flex-col gap-12 rounded-lg bg-[#d9d9d9] p-8 shadow-lg md:w-1/2 lg:w-1/4">
+        <h2 className="text-center text-3xl font-bold text-black subpixel-antialiased">
+          LOGIN
+        </h2>
+
+        <LoginForm />
+
+        <div className="flex flex-col items-center gap-4">
+          <Link
+            to="/sobre-ttg"
+            className="text-center text-sm text-gray-700 underline hover:text-[#005EAD]"
+          >
+            Saiba mais sobre a TTG e o SENAI CIMATEC
+          </Link>
+
+          <img
+            src="src/assets/cimatec-universidade-logo.png"
+            alt="SENAI CIMATEC"
+            className="mt-2 w-48"
+          />
+        </div>
+      </main>
+    </div>
   );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
 
+// Importe suas páginas
+import App from "./App.tsx"; // App agora será apenas a TELA DE LOGIN
 import DashboardGeralProjetos from "./pages/dashboard_geral_projetos.tsx"; 
 import CadastroAluno from "./pages/cadastros/cadastro_aluno.tsx";
+import AboutTTG from "./components/shared/AboutTTG"; // Importe o About aqui
 
 const router = createBrowserRouter([
   {
@@ -10,14 +12,17 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
+    path: "/sobre-ttg",
+    element: <AboutTTG />,
+  },
+  {
     path: "/dashboard/projetos",
     element: <DashboardGeralProjetos />,
   },
   {
-  path: "/orientador/cadastro/aluno",
-   element: <CadastroAluno/>,
+    path: "/orientador/cadastro/aluno",
+    element: <CadastroAluno />,
   },
-
 ]);
 
 export default router;
